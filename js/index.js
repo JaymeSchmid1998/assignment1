bf885441e32a612c01402997c3abfabffaec1fe4
jayme schmid <jayme.schmid@gmail.com>
2019-05-05 22:18:09 -0700

    finnished prototype

--- js/index.js
+++ js/index.js
@@ -23,21 +23,19 @@ var app = {
         
     },
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
+    //this is the device ready handler 
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
+        //this is the pause event 
          document.addEventListener("pause", this.pause, false);
+        //this is the resume event
     document.addEventListener("resume", this.resume, false);
-       // document.getElementById("add").addEventListener("vclick", function() {
-          //  console.log("this is working");
-            
-        //};
+      
+        
+        //this opens/creates the db and tables associated with it
         this.createDb();
         
-       // this.addTest();
+       
         
         
     },
@@ -47,36 +45,19 @@ var app = {
     },
     
     
+ 
     
     
     
     
-    
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+    //this is the pause function
    pause:function(){
         // location.replace("#first");
     console.log("in the pause ");
     },
+    //this is the on resume function 
         resume:function(){
+            //goes to the first page when the application is resumed 
           location.replace("#first");
     console.log("in the resme  ");
     },
@@ -89,8 +70,7 @@ var app = {
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
 
-        //listeningElement.setAttribute('style', 'display:none;');
-        //receivedElement.setAttribute('style', 'display:block;');
+        
 
         console.log('Received Event: ' + id);
         
@@ -103,23 +83,24 @@ var app = {
         
     },
     
-    
+    //this function creates the db
     createDb:function(){
      
      try{
+         //these are the values for the db
      var db= null;
     var name="T1";
     var version=1.0;
     var displayName="t1";
-   
+   //opening the db
     db = window.openDatabase( 'mydb','1.0', 'test1', 2024);
     console.log(db);
-                 alert('Sqlite Database created');
+                 //alert('Sqlite Database created');
         console.log('sql created');
     
           try{
     
-          
+          //a transaction of the db which creates the table to store the data in and also defines the columbs
  db.transaction(function(tx) {
        tx.executeSql("CREATE TABLE IF NOT EXISTS task3 (id integer primary key, TaskTitle varchar(255),TaskType varchar(255),date varchar(255),time varchar(255),IfDone varchar(255),Location varchar(255),ImgLoc varchar(355) )");
         
@@ -127,7 +108,7 @@ var app = {
     }, function(err){
         alert("An error occurred while initializing the app");
     });
-            alert('Sqlite TABLE created');
+            //alert('Sqlite TABLE created');
          console.log('sql TABLE created');
           
    
@@ -173,46 +154,36 @@ var app = {
     
    
 };
+//these are variables that hold the person location and also the image location
   var t1;
   var imageU;
-
+//this is a function that adds a task to the db
       function addnew() {
      console.log("this is working");
 
-     //this gets the elements from the page
+     //this gets the elements from the page like the tasks title and date. 
   var TaskTitle = document.getElementById("TaskTitle").value;
     var TaskType = document.getElementById("TaskType").value;
  var Date = document.getElementById("Date").value;
       var Time = document.getElementById("Time").value;
           var location;
-          /*try{
-            location = document.getElementById("locAdd").value;
-          }
-          catch(e){
-     alert(e);
-        console.log(e);
-              location="na";
-    } */
-          //alert(t1);
+          
           location=t1;
           //alert (location);
-              var Picture = document.getElementById("Time").value;
-     //var TaskTitle="a";
-         // var TaskType="a";
-         // var Date="1";
-         // var Time="1";
           
+              //var Picture = document.getElementById("Time").value;
+   
           
-          
+          //this obtains the ima
           var urlImg;
           urlImg=imageU;
-          alert(urlImg);
+        //  alert(urlImg);
           
           
           
           
      
-    //this checks for empty or null values  
+    //this checks for empty or null values or undef 
     if(TaskTitle == "" || TaskTitle == null)
     {
         alert("Please enter a title");
@@ -235,7 +206,7 @@ var app = {
         return;
     }
           else if(location==""||location==null|| location==undefined){
-             alert("location is null");
+             alert("please enter a Location");
         return; 
           }
     else{ 
@@ -246,7 +217,7 @@ var app = {
      var done ="no";
      var db = openDatabase( 'mydb','1.0', 'test1', 2024);
      
-
+//this transaction  inserts into the table 
    db.transaction(function(tx) {
       
         /*tx.executeSql("SELECT TaskTitle,COUNT(*)FROM task3 GROUP BY TaskTitle HAVING COUNT(*) > 1 VALUES (?) ", [TaskTitle], function(tx,res){
@@ -255,7 +226,7 @@ var app = {
         });*/
        
        
-       
+       //insert sql statment
         tx.executeSql("INSERT INTO task3 (TaskTitle, TaskType,date,time,IfDone,Location,ImgLoc) VALUES (?,?,?,?,?,?,?)", [TaskTitle, TaskType,Date,Time,done,location,urlImg], function(tx,res){
             alert("Task added");    
         });
@@ -274,20 +245,21 @@ var app = {
     }
 };
 //this should be run when ever the user ticks the done checkbox 
+//this updates undone tasks 
  function UpdateDone(TaskTitle) {
         
  console.log(TaskTitle);
      
      try{
      
-     
+     //changes the if done value from no to yes
      var done ="yes";
      var db = openDatabase( 'mydb','1.0', 'test1', 2024);
      
-
+//this tranasction updates the field on certain tasks from no to yes 
    db.transaction(function(tx) {
         tx.executeSql("update task3 set IfDone=? where TaskTitle=?", [done,TaskTitle], function(tx,res){
-            alert("updated");    
+            alert("Task marked as done");    
             console.log("this functioned correctly");
         });
     }, function(err){
@@ -303,20 +275,21 @@ var app = {
          
     
 };
+//this does the opposite and unmarks already marked tasks 
  function UpdateNotDone(TaskTitle) {
         
  console.log(TaskTitle);
      
      try{
      
-     
+     //this the varible that is used to change a ifdone from yes to no
      var done ="no";
      var db = openDatabase( 'mydb','1.0', 'test1', 2024);
      
-
+//this is the statement that changes a specific tasks ifdone from a yes to a no
    db.transaction(function(tx) {
         tx.executeSql("update task3 set IfDone=? where TaskTitle=?", [done,TaskTitle], function(tx,res){
-            alert("updated");    
+            alert("Task unmarked as done");    
             console.log("this functioned correctly");
         });
     }, function(err){
@@ -345,11 +318,11 @@ var app = {
 
 
 
-
+//this function deletes all of the tasks that are done 
  function DeleteTask() {
      console.log("this is working");
 
-    
+    //the variable that is inserted in the sql query
      var IfDone="yes";
          
  
@@ -357,13 +330,13 @@ var app = {
      try{
      
      
-    
+    //opens the db
      var db = openDatabase( 'mydb','1.0', 'test1', 2024);
      
-
+//this sql statment  deletes all of the tasks that are marked as being done 
    db.transaction(function(tx) {
         tx.executeSql("delete from task3  where IfDone=?", [IfDone], function(tx,res){
-            alert("deleted ");    
+            alert("Deleted all done tasks ");    
             console.log("delete functioned correctly");
         });
     }, function(err){
@@ -379,7 +352,7 @@ var app = {
          
     
 };
-
+//this function is used to display all of the tasks on the view task screen 
  function DisplayList() {
      console.log("this is working");
 
@@ -391,10 +364,10 @@ var app = {
      try{
      
      
-    
+    //opens the db 
      var db = openDatabase( 'mydb','1.0', 'test1', 2024);
      
-
+//this transaction is used to retreive all of the task values from the db
    db.transaction(function(tx) {
         tx.executeSql("select TaskTitle,date FROM task3", [], function(tx,res){
             var ROWS=res.rows;
@@ -426,13 +399,13 @@ var app = {
     
 };
 
-
+//this function is used to get the geo location of the person who is using the phone
  function GetGeo12() {
         
  
      
      try{
-          
+          //gets the longitude and the latitude 
             var onSuccess = function(position) {
         //alert('Latitude: '          + position.coords.latitude          + '\n' +'Longitude: '         + position.coords.longitude         + '\n');
           t1=" "+"latitude: "+ position.coords.latitude  + " " +"Longitude: "+ position.coords.longitude+ " ";
@@ -448,7 +421,7 @@ var app = {
     function onError(error) {
         alert('message: ' + error.message + '\n');
     }
-
+//this function is used to get the current geo location of the user 
    var nav= navigator.geolocation.getCurrentPosition(onSuccess, onError);  
                 //console.log(nav);
      
@@ -469,8 +442,9 @@ var app = {
 
 
 
-
+//this function is used to take the photo
 function takephoto (){
+    //this defines properties of the photo
                     let opts = {
                         quality: 80,
                         destinationType: Camera.DestinationType.FILE_URI,
@@ -482,21 +456,22 @@ function takephoto (){
                         targetHeight: 400
                     };
                     console.log("t1");
-                    navigator.camera.getPicture(ftw, wtf, opts);
+    //this function is used to take the photo
+                    navigator.camera.getPicture(takep1, takep2, opts);
                 };
-               function ftw(imgURI){
+               function takep1(imgURI){
                    console.log("t3"); document.getElementById('msg').textContent = imgURI;
                     document.getElementById('photo').src = imgURI;
                    imageU=imgURI;
                    
                     
                 };
-              function  wtf(msg){
+              function  takep2(msg){
                    console.log("t2"); document.getElementById('msg').textContent = msg;
                 };
             
 
-
+//this function deletes all of the tasks in the table
 function DeleteAll(){
                       try{
      
@@ -504,7 +479,7 @@ function DeleteAll(){
     
      var db = openDatabase( 'mydb','1.0', 'test1', 2024);
      
-
+//this is the sql query that is used to delete all of the tasks in the table 
    db.transaction(function(tx) {
         tx.executeSql("delete FROM task3", [], function(tx,res){
           alert("all tasks deleted");
